40da4e5ec3b6a86ef70938a16177dc69821103d1
Parth Tank <parthtank301@gmail.com>
2025-03-16 00:23:14 +0530

    FEAT: route prefix example

--- app/routes.ts
+++ app/routes.ts
@@ -2,13 +2,15 @@ import {
   type RouteConfig,
   index,
   layout,
+  prefix,
   route,
 } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("about", "routes/about.tsx"),
   route("post/:postId", "routes/post.tsx"),
+  // Prefix Example
+  ...prefix("some-route", [route("about", "routes/about.tsx")]),
   // Layout Example
   layout("routes/dashboard-layout.tsx", [
     // Nested Routes Example

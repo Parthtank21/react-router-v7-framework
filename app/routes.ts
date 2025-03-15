04ab1a132315c2e77d0b724c509bf225b07774e4
Parth Tank <parthtank301@gmail.com>
2025-03-16 00:10:03 +0530

    FEAT: nested routes

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,9 @@ export default [
   index("routes/home.tsx"),
   route("about", "routes/about.tsx"),
   route("post/:postId", "routes/post.tsx"),
+  // Nested Routes
+  route("dashboard", "routes/dashboard.tsx", [
+    route("finances", "routes/finances.tsx"),
+    route("personal", "routes/personal-info.tsx"),
+  ]),
 ] satisfies RouteConfig;
